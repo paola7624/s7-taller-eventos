b1c5e1a3f42abee70f0473280d60433436cb04b4
Paola <paolaperdomo555@gmail.com>
2024-08-12 14:56:01 -0300

    Solución - Propagación de evento

--- eventos.js
+++ eventos.js
@@ -1,4 +1,9 @@
-document.getElementById('div').addEventListener ('click', function() {
+document.getElementById('mi-div').addEventListener ('click', function() {
         alert('Hola! Soy un div');
     }
+);
+
+document.getElementById('mi-boton').addEventListener ('click', function(evento) {
+    evento.stopPropagation();;
+}
 );
